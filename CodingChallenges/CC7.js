fbd3bd645b8ee70aca51f73e30df9a5ced67c736
Vincent Futrell <vincent.futrell@gmail.com>
2022-06-17 12:56:20 -0600

    Moved calcBMI into the object

--- CodingChallenges/CC7.js
+++ CodingChallenges/CC7.js
@@ -20,17 +20,27 @@ function main() {
     const mark = {
         fullName: "Mark Miller",
         mass: 78,
-        height: 1.69
+        height: 1.69,
+        calcBMI: function(){
+            const BMI = this.mass / (this.height ** 2);
+            this.BMI = BMI;
+            return BMI;
+        },
     };
 
     const john = {
         fullName: "John Smith",
         mass: 92,
-        height: 1.95
+        height: 1.95,
+        calcBMI: function(){
+            const BMI = this.mass / (this.height ** 2);
+            this.BMI = BMI;
+            return BMI;
+        },
     }; 
 
-    const johnBMI = calcBMI(john);
-    const markBMI = calcBMI(mark);
+    const johnBMI = john.calcBMI();
+    const markBMI = mark.calcBMI();
 
     if(johnBMI > markBMI) console.log(`${john.fullName}'s BMI (${johnBMI}) is higher than ${mark.fullName}'s (${markBMI})`);
     else if(markBMI > johnBMI) console.log(`${mark.fullName}'s BMI (${markBMI}) is higher than ${john.fullName}'s (${johnBMI})`);
