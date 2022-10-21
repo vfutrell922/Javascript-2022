8b9f41209f437b997c9d6805a1666177056358dd
Vincent Futrell <vincent.futrell@gmail.com>
2022-10-21 15:52:56 -0600

    Checkbox toggle functioning properly

--- MSDropDown/MSDD.js
+++ MSDropDown/MSDD.js
@@ -2,23 +2,26 @@ import {MSDDElement} from "./MSDDElement.js";
 
 class MultiSelectDropDown extends HTMLElement
 {
-    selectedValuesList;
-    searchBar;
-    elementContainer;
+    #selectedValuesList;
+    #searchBar;
+    #elementContainer;
 
     constructor(values)
     {
         super(); 
         values = ["dog", "cat", "bird"]; //TODO: DELETE ME!!!
-        this.selectedValuesList = new Array();
+        this.#selectedValuesList = [];
         this.id = 'DropDown'
         this.classList = ('drop-down');
+        this.addEventListener('toggleSelected', (e) => {
+            this.updateSelectedList(e);
+        });
 
-        this.searchBar = document.createElement("input");
-        this.elementContainer = document.createElement("div");
+        this.#searchBar = document.createElement("input");
+        this.#elementContainer = document.createElement("div");
 
-        this.appendChild(this.searchBar);
-        this.appendChild(this.elementContainer);
+        this.appendChild(this.#searchBar);
+        this.appendChild(this.#elementContainer);
 
         this.#buildList(values);
     }
@@ -27,19 +30,36 @@ class MultiSelectDropDown extends HTMLElement
     {
         values.forEach(element => {
             let newElement = new MSDDElement(element);
-            this.elementContainer.appendChild(newElement);
+            this.#elementContainer.appendChild(newElement);
         });
     }
+
+    updateSelectedList(e)
+{
+    let value = e.detail.value;
+    if(this.#selectedValuesList.includes(value))
+    {
+        let index = this.#selectedValuesList.indexOf(value);
+        this.#selectedValuesList.splice(index, 1);
+    }
+    else
+    {
+        this.#selectedValuesList.push(value);
+    }
+
+    console.log(this.#selectedValuesList);
+}
 
     /**
      * Returns an array of MSDDElement.value for each selected element
      */
-    getSelected()
+    getSelectedValues()
     {
-        return this.selectedValuesList;
+        return this.#selectedValuesList;
     }
 
 }
+
 
 customElements.define("multi-select-drop-down", MultiSelectDropDown);
 
--- MSDropDown/MSDDElement.js
+++ MSDropDown/MSDDElement.js
@@ -1,38 +1,43 @@
 class MSDDElement extends HTMLElement
 {
-    value = "";
-    selected = false;
+    #value;
+    #selected;
     #title;
     #checkBox;
-
-    constructor(value)
+    
+    constructor(val)
     {
         super();
         this.classList = ('drop-down-element');
-        this.value = value;
+        this.#value = val;
+        this.#selected = false;
         this.#title = document.createElement('p');
-        this.#title.innerHTML = value;
+        this.#title.innerHTML = val;
         this.#checkBox = document.createElement('input');
         this.#checkBox.type = 'checkbox'
-        this.#checkBox.onclick = this.toggleSelected;
+        this.#checkBox.onclick = () => {
+            this.#toggleSelected(this);
+        };
         
         this.appendChild(this.#checkBox);
         this.appendChild(this.#title);
     }
 
-    toggleSelected()
+    #toggleSelected()
     {
-        this.selected = !this.selected;
+        this.#selected = !this.#selected;
+        const event = new CustomEvent('toggleSelected', {detail: {value: this.#value}, bubbles: true});
+        this.dispatchEvent(event);
     }
 
     isSelected()
     {
-        return this.selected;
+        return this.#selected;
     }
 
     getValue()
     {
-        return this.value;
+        return this.#value;
     }
 }
 
